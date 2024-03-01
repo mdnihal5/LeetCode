f6844627f35c6b665e5aaf90122b66023ad58c55
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2024-03-01 21:44:11 +0530

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -1,57 +1,98 @@
-#include<bits/stdc++.h>
-#define int long long
-
-using namespace std;
-
-bool compare(const pair<int, int>& a,
-                const pair<int, int>& b)
-{
-    if (a.first != b.first)
-        return (a.first < b.first);
-    else
-       return (a.second > b.second);
-}
-
-int32_t main(){
-    int tt=1;
-    cin>>tt;
-    while(tt--){
-        int n;
-        cin>>n;
-        string s;
-        cin>>s;
-        vector<int> v;
-        vector<pair<int,int>> p;
-
-        for(int i=0;i<n;i++) p.push_back({(int)s[i],i});
-        sort(p.begin(),p.end(),compare);
-        reverse(p.begin(),p.end());
-        int last = -1;
-
-        for(int i=0;i<n;i++){
-            if(p[i].second < last) continue;
-            v.push_back(p[i].second);
-            last = p[i].second;
-        }
-        string s3="";
-        int m = v.size();
-        for(int i=0;i<m;i++) s3 += s[v[i]];
-        reverse(s3.begin(),s3.end());
-        for(int i=0;i<m;i++){
-            s[v[i]] = s3[i];
-        }
-        string s2 = s;
-        sort(s2.begin(),s2.end());
-        int cnt=0;
-        reverse(s3.begin(),s3.end());
-        while(1){
-            if(cnt == m) break;
-            if(s3[cnt] == s3[0]) cnt++;
-            else break;
-        }
-
-        int ans = m - cnt;
-        if(s == s2) cout<<ans<<endl;
-        else cout<<-1<<endl;
-    }
+#include <bits/stdc++.h> 
+ 
+using namespace std; 
+ 
+// #define int long long 
+#define ff first 
+#define ss second 
+#define pb push_back 
+#define vi vector<int> 
+#define qi queue<int> 
+#define mii map<int, int> 
+#define pii pair<int, int> 
+#define mip map<int, pair<int, int>> 
+#define double long double 
+#define FOR(i, k, n) for (int i = k; i < n; i++) 
+#define all(a) a.begin(), a.end() 
+#define rall(a) a.rbegin(), a.rend() 
+#define double long double 
+#define get(a)        \ 
+    for (auto &i : a) \ 
+    cin >> i 
+#define put(a)        \ 
+    cout << endl;     \ 
+    for (auto &i : a) \ 
+    cout << i << " " 
+#define ps(x, y) fixed << setprecision(y) << x 
+#define endl "\n" 
+#define sp << " " << 
+#define P(x) cout << x << endl 
+ 
+const int mod = 1000000007; 
+ 
+void solve() 
+{ 
+    int n, m; 
+    cin >> n >> m; 
+    cout << "? 1 1" << endl; 
+    int a; 
+    cin >> a; 
+    a += 2; 
+    cout << "? " << n << " 1" << endl; 
+    int b; 
+    cin >> b; 
+    b -= (n - 1); 
+    b = -b; 
+    int x = (a + b) / 2; 
+    int y = a - x; 
+    if (x >= 1 && x <= n && y >= 1 && y <= m) 
+    { 
+        cout << "? " << x << " " << y << endl; 
+        int out; 
+        cin >> out; 
+        if (out == 0) 
+        { 
+            cout << "! " << x << " " << y; 
+        } 
+        else 
+        { 
+            cout << "? " << n << " " << m << endl; 
+            int c; 
+            cin >> c; 
+            c -= (n + m); 
+            c = -c; 
+            a = c; 
+            x = (a + b) / 2; 
+            y = a - x; 
+            cout << "! " << x << " " << y; 
+        } 
+    } 
+    else 
+    { 
+        cout << "? " << n << " " << m << endl; 
+        int c; 
+        cin >> c; 
+        c -= (n + m); 
+        c = -c; 
+        a = c; 
+        x = (a + b) / 2; 
+        y = a - x; 
+        cout << "! " << x << " " << y; 
+    } 
+} 
+signed main() 
+{ 
+    // ios::sync_with_stdio(0); 
+    // cin.tie(0); 
+    // cout.tie(0); 
+    int i = 1; 
+    int t = 1; 
+    cin >> t; 
+    while (t--) 
+    { 
+        // cout<<"Case #"<<i++<<": "; 
+        solve(); 
+        cout << endl; 
+    } 
+    return 0; 
 }
